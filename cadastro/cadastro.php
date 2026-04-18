bf4814dc57c125e98956e90d549985383425865f
thiagoassis15 <thiagodasilvaassis14@gmail.com>
2026-04-18 13:02:51 -0300

    quinto commit - thiago

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -6,6 +6,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nomeCompleto = $_POST['nome'];
     $idade = $_POST['idade'];
     $email = $_POST['email'];
+    $cep = $_POST['cep'];
+    $casa_num = $_POST['casa_num'];
     $senha = $_POST['senha'];
     
     $partes = explode(' ', $nomeCompleto);
